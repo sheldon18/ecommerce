77939e04e4c87c4160927ecb0cf0883cd3b20798
Ubuntu <ubuntu@ip-172-31-81-189.ec2.internal>
2020-03-16 01:43:20 +0000

    import env changed in settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 
 AWS_S3_OBJECT_PARAMETERS = {
     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-    'CacheControl': 'max-age=94608000'
+    'CacheControl': 'max-age=94608000',
 }
 
 AWS_STORAGE_BUCKET_NAME = 'sheldons-ecommerce'
